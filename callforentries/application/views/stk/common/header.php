8ce67a3a7447e0bf6e2f76fafbed71d1bed9a93e
Kaushtuv <kaushtuvgurung@gmail.com>
2014-10-17 11:00:27 +1100

    header viewport

--- callforentries/application/views/stk/common/header.php
+++ callforentries/application/views/stk/common/header.php
@@ -1,7 +1,9 @@
 <!doctype html>
 <html>
 <head>
-<meta charset="UTF-8">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
 <!-- jQuery -->
 <script type="text/javascript" src="<?=base_url()?>js/jquery-1.10.2.js"></script>
